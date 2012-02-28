daf190b08dc3a6ab6df94300e146e8a89d590c53
Tom Parker <palfrey@tevp.net>
2012-02-28 22:43:05 +0000

    Add options support

--- generate.py
+++ generate.py
@@ -2,18 +2,31 @@ from genshi.template.text import NewTextTemplate
 from os import getenv, makedirs, walk, stat, chmod
 from os.path import exists, join, dirname
 from time import strftime, gmtime
+from optparse import OptionParser
 
-name = "HelloWorld"
-description = "Timetracker applet"
-icon = "time-admin.png"
-category = "Utility"
-scriptRoot = "/usr/lib/gnome-applets/"
-#parser.add_option("-n","--name",dest="name",default=getenv("debfullname","default name"), help="Maintainer name (Default: %s)"%getenv("DEBFULLNAME","Default name"))
-maintainer = getenv("debfullname","default name")
-#parser.add_option("-e","--email",dest="email",default=getenv("DEBEMAIL","fixme@invalid_email_path"),help="Maintainer email (Default: %s)"%getenv("DEBEMAIL","fixme@invalid_email_path"))
-email = getenv("DEBEMAIL","fixme@invalid_email_path")
+parser = OptionParser(description="Panel applet generator")
+
+parser.add_option("-n", "--name", dest="name", help="Applet name (required)", default=None)
+parser.add_option("-d", "--description", dest="description", help="Description (required)", default=None)
+parser.add_option("-i", "--icon", dest="icon", default= "time-admin.png", help="Icon (in /usr/share/icons, without the full path)")
+parser.add_option("-c", "--category", dest="category", help="Category of the applet (see http://standards.freedesktop.org/menu-spec/latest/apa.html for full list)", default = "Utility")
+parser.add_option("--maintainer",dest="maintainer",default=getenv("debfullname","default name"), help="Maintainer name (Default: %s)"%getenv("DEBFULLNAME","Default name"))
+parser.add_option("-e","--email",dest="email",default=getenv("DEBEMAIL","fixme@invalid_email_path"),help="Maintainer email (Default: %s)"%getenv("DEBEMAIL","fixme@invalid_email_path"))
+parser.add_option("-f", "--folder", help="Destination folder (default: same as the name)", dest="folder", default=None)
+
+(opts, args) = parser.parse_args()
+if args != []:
+	parser.error("Extra arguments after named parameters! %s"%(",".join(args)))
+if opts.name == None:
+	parser.error("Need a name for the applet!")
+if opts.description == None:
+	parser.error("Need a description for the applet!")
+
+name = opts.name
+if opts.folder == None:
+	opts.folder = name
 now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-folder = name
+scriptRoot = "/usr/lib/gnome-applets"
 
 templates = [
 		("applet.py", "%sApplet.py" % name),
@@ -31,20 +44,20 @@ for root,dirs,files in walk("templates/debian"):
 
 for (src, dest) in templates:
 	src = join("templates", src)
-	dest = join(folder, dest)
+	dest = join(opts.folder, dest)
 	destdir = dirname(dest)
 	if destdir != "" and not exists(destdir):
 		makedirs(destdir)
 	tmpl = NewTextTemplate(open(src).read())
 	stream = tmpl.generate(
-			name = name,
+			name = opts.name,
 			scriptRoot = scriptRoot,
-			description = description,
-			icon = icon,
-			category = category,
-			lowerName = name.lower(),
-			maintainer = maintainer,
-			email = email,
+			description = opts.description,
+			icon = opts.icon,
+			category = opts.category,
+			lowerName = opts.name.lower(),
+			maintainer = opts.maintainer,
+			email = opts.email,
 			now = now)
 	open(dest, "wb").write(stream.render())
 
