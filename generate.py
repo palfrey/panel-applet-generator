1e140a0c0dd35bdf12124f92b937988b04a783f8
Tom Parker <palfrey@tevp.net>
2012-02-28 22:25:35 +0000

    Add debian templates

--- generate.py
+++ generate.py
@@ -1,11 +1,19 @@
-from genshi.template import TemplateLoader
 from genshi.template.text import NewTextTemplate
+from os import getenv, makedirs, walk, stat, chmod
+from os.path import exists, join, dirname
+from time import strftime, gmtime
 
-name = "Timetracker"
+name = "HelloWorld"
 description = "Timetracker applet"
 icon = "time-admin.png"
 category = "Utility"
-path = "/home/palfrey/src/timetracker-widget/applet.py"
+scriptRoot = "/usr/lib/gnome-applets/"
+#parser.add_option("-n","--name",dest="name",default=getenv("debfullname","default name"), help="Maintainer name (Default: %s)"%getenv("DEBFULLNAME","Default name"))
+maintainer = getenv("debfullname","default name")
+#parser.add_option("-e","--email",dest="email",default=getenv("DEBEMAIL","fixme@invalid_email_path"),help="Maintainer email (Default: %s)"%getenv("DEBEMAIL","fixme@invalid_email_path"))
+email = getenv("DEBEMAIL","fixme@invalid_email_path")
+now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+folder = name
 
 templates = [
 		("applet.py", "%sApplet.py" % name),
@@ -13,12 +21,32 @@ templates = [
 		("dbus-service.template", "org.gnome.panel.applet.%s.service" % name),
 		("factory2.py", "%s-factory2.py" % name),
 		("factory3.py", "%s-factory3.py" % name),
-		("panel-applet.template", "org.gnome.applets.%s.panel-applet" % name)
+		("panel-applet.template", "org.gnome.applets.%s.panel-applet" % name),
 		]
 
-loader = TemplateLoader("templates")
+for root,dirs,files in walk("templates/debian"):
+	for f in files:
+		fullpath = join(root,f)[len("templates/"):]
+		templates.append((fullpath, fullpath))
 
 for (src, dest) in templates:
-	tmpl = loader.load(src, cls=NewTextTemplate)
-	stream = tmpl.generate(name=name, path=path, description=description, icon=icon, category=category)
+	src = join("templates", src)
+	dest = join(folder, dest)
+	destdir = dirname(dest)
+	if destdir != "" and not exists(destdir):
+		makedirs(destdir)
+	tmpl = NewTextTemplate(open(src).read())
+	stream = tmpl.generate(
+			name = name,
+			scriptRoot = scriptRoot,
+			description = description,
+			icon = icon,
+			category = category,
+			lowerName = name.lower(),
+			maintainer = maintainer,
+			email = email,
+			now = now)
 	open(dest, "wb").write(stream.render())
+
+	bits = stat(src).st_mode
+	chmod(dest, bits)
